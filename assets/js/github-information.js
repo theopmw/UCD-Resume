a156cda88310085e518aae6a7e3b153db8c1f22e
theopmw <theopmw@gmail.com>
2021-03-17 11:11:17 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -11,8 +11,37 @@ function userInformationHTML(user) {
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+            <p>Followers: ${user.followers} <br> Following: ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length === 0) {
+        // Returns "No repos!" message if the user has 0 repos:
+        return `<div class="clearfix repo-list">No repos!</div>` 
+    }
+
+    let listItemsHTML = repos.map(function(repo){ // The map method works like a forEach but it returns an array with the results of this function
+        // The contents of the array that we want to return are: 
+        // an <li> list item 
+        // inside the list item an <a> anchor tag that will take us to the actual repo when we click on it ({repo.html_url})
+        // the text inside the <a> tag that is displayed to the user will be the repo name ({repo.name})
+        return `<li> 
+                    <a href="${repo.html_url}" target="_blank" rel="noopener">${repo.name}</a>
+                </li>`;
+    });
+    // Returns list of repos if user has 1 or more repos:
+    // <ul> will be the parent for all the list items created above
+    // As the map function used above returns an array, we use the join method (${listItemsHTML.join("\n")}) on the listItemsHTML array and join everything with a new line ("\n") - this stops us havinf to iterate through the new array once again.
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`
+
 }
 
 function fetchGitHubInformation(event) {
@@ -28,12 +57,15 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-    $.when
-        ($.getJSON(`https://api.github.com/users/${username}`)
+    $.when (
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then (
-        function (response) {
-            let userData = response;
+        function (firstResponse, secondResponse) {
+            let userData = firstResponse[0];
+            let repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
